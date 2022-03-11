22d54db3cdf6db4080f1b5db54cada9b1f23ffbc
NemaGrijiVelkataDviji <vili.levski@abv.bg>
2022-03-11 13:50:13 +0200

    Space Invaders basic layout, Boss loading and spawning, Enemy loading and spawning, Player shooting base

--- SpaceInvaders/SpaceInvaders.js
+++ SpaceInvaders/SpaceInvaders.js
@@ -1,8 +1,16 @@
 const KEY_RIGHT = 39;
 const KEY_LEFT = 37;
+const FIRE_KEY = 70;
+const DIRECTION_DOWN = -1;
+const DIRECTION_UP = 1;
+
+var n;
 
 const GAME_WIDTH = 1300;
 const GAME_HEIGHT = 900;
+
+bosses = [];
+enemies = [];
 
 const STATE = {
     x_pos: 0,
@@ -19,6 +27,137 @@ function setSize($element, width) {
     $element.style.height = "auto";
 }
 
+window.onload = function(){
+    //loadBosses();
+    //spawnBoss();
+};
+
+function loadBosses(){
+    let boss1 = {
+        img: '../images/red_ship-removebg-preview.png',
+        health: 100,
+        damage: 50,
+        class: 'className'
+    };
+    bosses.push(boss1);
+
+    let boss2 = {
+        img: '../images/red_ship-removebg-preview.png',
+        heath: 150,
+        damage: 35,
+        class: 'className'
+    };
+
+    bosses.push(boss2);
+
+    
+    let boss3 = {
+        img: '../images/red_ship-removebg-preview.png',
+        heath: 120,
+        damage: 42,
+        class: 'className'
+    };
+    
+    bosses.push(boss3);
+}
+
+function loadEnemies(){
+    let enemy1 = {
+        img: 'imagePath1'
+    }
+    
+    let enemy2 = {
+        img: 'imagePath1'
+    }
+    
+    let enemy3 = {
+        img: 'imagePath1'
+    }
+    
+    let enemy4 = {
+        img: 'imagePath1'
+    }
+    
+    let enemy5 = {
+        img: 'imagePath1'
+    }
+
+    enemies.push(enemy1);
+    enemies.push(enemy2);
+    enemies.push(enemy3);
+    enemies.push(enemy4);
+    enemies.push(enemy5);
+}
+
+function generateRandom(maxLimit){
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand);
+    return rand;
+}
+
+function spawEnemy(){
+    const $box = document.querySelector(".game");
+    let index = generateRandom(enemies.length);
+    let enemyImg = enemies[index];
+    let enemy = document.createElement('img');
+    enemy.src = enemyImg.img;
+    $box.appendChild(enemy);
+}
+
+function spawnBoss(){
+    let bossIndex = generateRandom(bosses.length);
+    const boss = document.createElement('img');
+    let currentBoss = bosses[bossIndex];
+    boss.src = '../images/red_ship-removebg-preview.png'; //currentBoss.img;
+    
+    const $box = document.querySelector(".game");
+    $box.appendChild(boss);
+}
+
+function playerControlls(key){
+    let keyNum;
+
+    if(window.event){
+        keyNum = key.keyCode;
+    } else if (key.which){
+        keyNum = key.which;
+    }
+
+    if(keyNum == FIRE_KEY){
+        let player = document.getElementsByClassName('player');
+        let x = STATE.x_pos;
+        let y = STATE.y_pos;
+        shoot(n, 1, x, y);
+        n++;
+    }
+}
+
+function shoot(id, direction, x, y){
+    let image = '../images/fireball-removebg-preview.png';
+    let imageItem = document.createElement('img');
+    imageItem.src = image;
+    imageItem.id = id;
+    imageItem.style.left = `${x}px`;
+    imageItem.style.top = `${y}px`; 
+    imageItem.style.position = 'relative';
+    imageItem.style.display = 'flex';
+    const $box = document.querySelector(".game");
+    setSize(imageItem, 50);
+    $box.appendChild(imageItem);
+    moveFireball(id, y);
+}
+
+function moveFireball(fireballId, startY){
+    let fireball = document.getElementById(fireballId);
+    for (let i = startY; i >= 0; i--){
+        fireball.style.top = `${i}px`;
+        setTimeout(function() {
+        }, 1000 / 30);
+    }
+    const $box = document.querySelector(".game");
+    $box.removeChild(fireball);
+    
+}
 
 //Player
 function createPlayer($box) {
@@ -26,7 +165,9 @@ function createPlayer($box) {
     STATE.y_pos = GAME_HEIGHT / 2;
 
     const $player = document.createElement("img");
-    $player.src = 'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+    $player.style.position = 'relative';
+    $player.style.display = 'flex';
+    $player.src = '../images/red_ship-removebg-preview.png';
     $player.className = "player";
 
     
@@ -37,5 +178,5 @@ function createPlayer($box) {
 
 
 //Game
-const $box = document.querySelector(".main");
+const $box = document.querySelector(".game");
 createPlayer($box);
